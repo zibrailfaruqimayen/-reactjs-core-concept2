0fa165695355229e5f16d8693ff4f4cd93361f43
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-21 22:34:28 +0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">{/* <Counter></Counter> */}</div>;
+  return (
+    <div className="App">
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
+    </div>
+  );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  // useEffect(()=>{}, [])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {users.map((user) => (
+        <User name={user.name} email={user.email}></User>
+      ))}
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div
+      style={{ border: "2px solid red", margin: "20px", borderRadius: "15px" }}
+    >
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  );
 }
 
 function Counter() {
